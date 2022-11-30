61ad092721e95dd2fc2b1d835559330f0983706b
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2022-11-30 23:36:48 +0100

    Spojili front i bek

--- iks_projekat_tim_24_uberApp/src/app/app.component.ts
+++ iks_projekat_tim_24_uberApp/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'iks_projekat_tim_24_uberApp';
+
+  public users: User[] = [];
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers(): void{
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
--- iks_projekat_tim_24_uberApp/src/app/app.module.ts
+++ iks_projekat_tim_24_uberApp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
