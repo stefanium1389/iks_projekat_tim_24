0c9760119db3f890a9d85ae0ebe4c669d19f4424
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-12-28 17:13:54 +0100

    feat: add search button and create reactive form for locations

--- iks_projekat_tim_24_uberApp/src/app/unregistered-user-main/unregistered-user-main.component.ts
+++ iks_projekat_tim_24_uberApp/src/app/unregistered-user-main/unregistered-user-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-unregistered-user-main',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnregisteredUserMainComponent implements OnInit {
 
-  constructor() { }
+  destinationForm: FormGroup;
+
+  constructor() {
+    this.destinationForm = new FormGroup({
+      start_location: new FormControl(),
+      end_location: new FormControl()
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  search(which : string) {
+    if (which === "start")
+    {
+      console.log(`pocetna sacuvana u komponenti je ${this.destinationForm.get('start_location')?.value}`)
+    }
+    else
+    {
+      console.log(`krajnja sacuvana u komponenti je ${this.destinationForm.get('end_location')?.value}`)
+    }
+    
+    /*const usernameControl = this.loginForm.get('start_location');
+    usernameControl?.setValue("angulu");*/
   }
 
 }
