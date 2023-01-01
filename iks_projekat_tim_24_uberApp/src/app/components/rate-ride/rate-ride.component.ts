d281a8f04607ceae2eed3c4feb1490c979ea5888
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-01-01 18:46:34 +0100

    Comment section added

--- iks_projekat_tim_24_uberApp/src/app/components/rate-ride/rate-ride.component.ts
+++ iks_projekat_tim_24_uberApp/src/app/components/rate-ride/rate-ride.component.ts
@@ -9,10 +9,20 @@ export class RateRideComponent implements OnInit {
 
   vozacRating:number=0;
   voziloRating:number=0;
+  vehicle:string="Ford Mondeo";
+  driver:string="Ime Prezime";
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  confirm(){
+
+  }
+  back(){
+    
   }
 
 }
