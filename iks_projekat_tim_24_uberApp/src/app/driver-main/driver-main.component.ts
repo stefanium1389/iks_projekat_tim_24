1d729edc87bdd079a7c67d8bb78480e66ccd4731
VladimirGolosin <vladimirgolosin1@gmail.com>
2023-01-13 13:43:22 +0100

    refactor: move logic to service

--- iks_projekat_tim_24_uberApp/src/app/driver-main/driver-main.component.ts
+++ iks_projekat_tim_24_uberApp/src/app/driver-main/driver-main.component.ts
@@ -1,20 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { JwtService } from '../components/jwt-service.service';
-
-interface WorkingHourResponse
-{
-  id:number;
-  start:string;
-  end:string;
-}
-
-interface ListOfWorkingHours {
-  totalCount: number;
-  results: Array<WorkingHourResponse>
-}
-
+import { DriverService } from '../services/driver.service';
 
 @Component({
   selector: 'app-driver-main',
@@ -28,58 +13,28 @@ export class DriverMainComponent implements OnInit {
   isDriverActive:boolean;
   inRide:boolean;
 
-  constructor(private http: HttpClient, private jwtService: JwtService) { }
+  constructor(private driverService: DriverService) { }
 
-  ngOnInit(): void {
-
-    this.initialCheckForWorkingHour();
-    
-}
-
-  //pita da li postoji aktivan wh za datog vozača, ako ima primamo listu od 1 elementa
-  initialCheckForWorkingHour()
-  { 
-    if (localStorage.getItem('userPausedWorkingHour'))
-    {
-      this.endDrivingSession();
-      return;
-    }
-    this.http.get<ListOfWorkingHours>(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/last-active-working-hour`)
-        .subscribe(
-            response => { this.initialOnResponse(response) },
-            error => { this.handleWorkingHourError(error) }
-        );
+  ngOnInit(): void 
+  {
+    this.driverService.statusChanged.subscribe(status => {
+      this.handleStatusChange(status);
+    });
+    this.driverService.initialCheckForWorkingHour();
   }
 
-  
-  initialOnResponse(response : ListOfWorkingHours)
-  {
-    if (response.totalCount === 0)
-    {
-      this.createNewWorkingHour();
+  handleStatusChange(status: string) {
+    if (status === 'active') {
+        this.activateDrivingSession();
+    } else if (status === 'inactive') {
+        this.endDrivingSession();
     }
-    this.activateDrivingSession();
-  }
-
-  createNewWorkingHour()
-  {
-    let date = new Date();
-    let isoDate = date.toISOString();
-    this.http.post(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/working-hour`, {start:isoDate} )
-        .subscribe(
-            response => { console.log("kreiran wh") },
-            error => { this.handleWorkingHourError(error) }
-        );
   }
 
   activateDrivingSession()
   {
     this.isDriverActive = true;
     this.activityStatus = "aktivan";
-    if (localStorage.getItem('userPausedWorkingHour'))
-    {
-      localStorage.removeItem('userPausedWorkingHour');
-    }
   }
 
   endDrivingSession()
@@ -88,50 +43,15 @@ export class DriverMainComponent implements OnInit {
     this.activityStatus = "neaktivan";
   }
 
-  handleWorkingHourError(error : any)
-  {
-    console.log("greška kod working hour");
-    console.log(error) //lel
-  }
-
   //ovome treba krpljenje xd
   onClickEndDriverHour()
   {
-    this.http.get<ListOfWorkingHours>(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/last-active-working-hour`)
-        .subscribe(
-            response => { this.endDrivingWorkingHour(response) },
-            error => { this.handleWorkingHourError(error) }
-        );
-  }
-
-  endDrivingWorkingHour(list: ListOfWorkingHours)
-  {
-    let date = new Date();
-    let isoDate = date.toISOString();
-    if (list.totalCount === 0)
-    {
-      alert("working hour nije pronadjen!!"); //zameniti sa exception-om
-    }
-
-    this.http.put(`${environment.apiBaseUrl}api/driver/working-hour/${list.results[0].id}`, {end:isoDate})
-        .subscribe(
-            response => { this.endDrivingSession() },
-            error => { this.handleWorkingHourError(error) }
-        );
-    //ovako govorimo sajtu da ne pravi novi wh pri refreshu
-    localStorage.setItem('userPausedWorkingHour','true');
-    this.endDrivingSession();
+    this.driverService.onClickEnd();
   }
 
   onClickStartDriverHour()
   {
-    let date = new Date();
-    let isoDate = date.toISOString();
-    this.http.post(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/working-hour`, {start:isoDate} )
-        .subscribe(
-            response => { this.activateDrivingSession() },
-            error => { this.handleWorkingHourError(error) }
-        );
+    this.driverService.onClickStart();
   }
 
   panic(){
---
+++ iks_projekat_tim_24_uberApp/src/app/services/driver.service.ts
@@ -0,0 +1,130 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtService } from '../components/jwt-service.service';
+import { environment } from 'src/environments/environment';
+import { EventEmitter } from '@angular/core';
+
+
+export interface WorkingHourResponse
+{
+  id:number;
+  start:string;
+  end:string;
+}
+
+export interface ListOfWorkingHours {
+  totalCount: number;
+  results: Array<WorkingHourResponse>
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverService {
+
+  statusChanged = new EventEmitter<string>();
+
+  constructor(private http: HttpClient, private jwtService: JwtService) { }
+
+  //pita da li postoji aktivan wh za datog vozača, ako ima primamo listu od 1 elementa
+  initialCheckForWorkingHour()
+  { 
+    if (localStorage.getItem('userPausedWorkingHour'))
+    {
+      this.setInactiveInComponent();
+      return;
+    }
+    this.http.get<ListOfWorkingHours>(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/last-active-working-hour`)
+        .subscribe(
+            response => { this.initialOnResponse(response) },
+            error => { this.handleWorkingHourError(error) }
+        );
+  }
+
+  initialOnResponse(response : ListOfWorkingHours)
+  {
+    if (response.totalCount === 0)
+    {
+      this.createNewWorkingHour();
+    }
+    this.setActiveInComponent(); //ovde bi možda trebalo koristiti subscribe
+  }
+
+  handleWorkingHourError(error : any)
+  {
+    console.log("greška kod working hour");
+    console.log(error) //lel
+  }
+  
+  createNewWorkingHour()
+  {
+    let date = new Date();
+    let isoDate = date.toISOString();
+    this.http.post(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/working-hour`, {start:isoDate} )
+        .subscribe(
+            response => { console.log("kreiran wh") },
+            error => { this.handleWorkingHourError(error) }
+        );
+  }
+
+  endDrivingWorkingHour(list: ListOfWorkingHours)
+  {
+    let date = new Date();
+    let isoDate = date.toISOString();
+    if (list.totalCount === 0)
+    {
+      alert("working hour nije pronadjen!!"); //zameniti sa exception-om
+    }
+
+    this.http.put(`${environment.apiBaseUrl}api/driver/working-hour/${list.results[0].id}`, {end:isoDate})
+        .subscribe(
+            response => { this.setInactiveInComponent() },
+            error => { this.handleWorkingHourError(error) }
+        );
+    
+    this.setInactiveInComponent();
+  }
+
+  setActiveInComponent() {
+    this.statusChanged.emit('active');
+  }
+
+  setInactiveInComponent() {
+    this.statusChanged.emit('inactive');
+  }
+
+  onClickEnd()
+  {
+    this.http.get<ListOfWorkingHours>(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/last-active-working-hour`)
+    .subscribe(
+        response => { this.endDrivingWorkingHour(response); this.addToLocal(); },
+        error => { this.handleWorkingHourError(error) }
+    );
+  }
+
+  onClickStart()
+  {
+    let date = new Date();
+    let isoDate = date.toISOString();
+    this.http.post(`${environment.apiBaseUrl}api/driver/${this.jwtService.getId()}/working-hour`, {start:isoDate} )
+        .subscribe(
+            response => { this.setActiveInComponent(); this.removeFromLocal()},
+            error => { this.handleWorkingHourError(error) }
+        );
+  }
+
+  removeFromLocal()
+  {
+    if (localStorage.getItem('userPausedWorkingHour'))
+    {
+      localStorage.removeItem('userPausedWorkingHour');
+    }
+  }
+
+  addToLocal()
+  {
+    localStorage.setItem('userPausedWorkingHour','true');
+  }
+
+
+}
