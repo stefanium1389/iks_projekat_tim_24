0572ddc1cd729edcab282f449cb559a6a219c2c0
VladimirGolosin <vladimirgolosin1@gmail.com>
2023-01-22 00:55:21 +0100

    feat: implement statistics calls to backend

--- iks_projekat_tim_24_uberApp/src/app/services/chart.service.ts
+++ iks_projekat_tim_24_uberApp/src/app/services/chart.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ChartType } from 'chart.js';
+import { ChartData, ChartType } from 'chart.js';
 import { Observable, of } from 'rxjs';
+import { JwtService } from '../components/jwt-service.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs';
+import { registerLocaleData } from '@angular/common';
+import { AdminViewingService } from './admin-viewing.service';
 
- export interface ChartToRender
-{
+export interface ChartToRender {
   type: ChartType;
-  data: Array<any>;
-  labels: Array<any>;
-  options: any;
-  total: string;
-  average: string;
+  data: ChartData;
+  total: number;
+  average: number;
 }
 
-export interface ChartRequest
-{
-  role: string;
+export interface ChartDTO {
+  type: string;
+  data: Array<any>;
+  labels: Array<string>;
+  total: number;
+  average: number;
+}
+
+export interface ChartRequest {
   topic: string;
   startDate: Date;
   endDate: Date;
@@ -25,60 +34,115 @@ export interface ChartRequest
 })
 export class ChartService {
 
-  constructor() { }
+  constructor(private jwtService: JwtService, private http: HttpClient, private adminViewingService: AdminViewingService) { }
 
-    public getChart( chartRequest: ChartRequest) : Observable<any>
-  {
-    //procesirati request, ovo je samo neki dummy
+  public getTypesOfChart() {
+    let role = this.jwtService.getRole();
 
-
-     let ChartType : ChartType = 'radar';
-     let ChartData : Array<any> = [
-      { data: [1000, 2000, 3000], label: 'dunno' }
-    ];;
-     let ChartLabels: Array<string> = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
-     let ChartOptions: any = {
-      responsive: true
-    };
-    let ChartTotal = "-100";
-    let ChartAverage = "-50.5";
-
-    if (chartRequest.topic === "kilometri")
-    {
-      ChartType = 'line';
-      ChartData = [
-       { data: [100, 590, 800, 810, 560, 550, 400], label: 'pređeni km' }
-     ];
+    if (role === "ADMIN") {
+      let adminViewingRole = this.adminViewingService.getAdminViewingRole();
+      if (adminViewingRole === null) {
+        return ["globalni broj vožnji", "globalni broj kilometara", "globalni troškovi"];
+      }
+      role = adminViewingRole;
     }
-    else if (chartRequest.topic === "troškovi")
-    {
-      ChartType = 'pie';
-      ChartData = [
-       { data: [1000, 5900, 8005, 8150, 5160, 1550, 2400], label: 'troškovi' }
-     ];
+    if (role === "DRIVER") {
+      return ["broj obavljenih vožnji", "broj voženih kilometara"];
     }
-    else if (chartRequest.topic === "vožnje")
-    {
-      ChartType = 'bar';
-      ChartData = [
-       { data: [1000, 5900, 8005, 8150, 5160, 1550, 2400], label: 'vožnje' }
-     ];
+    if (role === "USER") {
+      return ["broj vožnji", "broj kilometara", "troškovi"];
     }
 
-    let response : ChartToRender = 
-    {
-      data : ChartData,
-      type : ChartType,
-      options : ChartOptions,
-      labels : ChartLabels,
-      total : ChartTotal,
-      average : ChartAverage,
-    }
-
-    return of(response);
+    throw new Error("problem in finding the role");
 
   }
 
+  getChart(chartRequest: ChartRequest): Observable<ChartToRender> {
+    let id: number | null;
+    let adminViewingId = this.adminViewingService.getAdminViewingId();
+    if (adminViewingId == null) {
+      id = this.jwtService.getId();
+      console.log("ADMIN VIEWING JE NULL");
+    }
+    else {
+      id = adminViewingId;
+      console.log("ADMIN VIEWING NIJE NULL");
+    }
+
+    let addressPiece: string = this.getAddress(chartRequest.topic, id);
+    console.error(environment.apiBaseUrl + `api/statistics/` + addressPiece);
+    return this.http.post<ChartDTO>(environment.apiBaseUrl + `api/statistics/` + addressPiece, { startDate: chartRequest.startDate, endDate: chartRequest.endDate }).pipe(
+      map(result => {
+        let chartType: ChartType;
+        switch (result.type) {
+          case 'pie':
+            chartType = 'pie';
+            break;
+          case 'line':
+            chartType = 'line';
+            break;
+          case 'bar':
+            chartType = 'bar';
+            break;
+          default:
+            throw new Error("chart type not found!");
+        }
+
+        let leData: ChartData = {
+          labels: result.labels,
+          datasets: [{
+            label: chartRequest.topic,
+            data: result.data,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        };
+
+        const response: ChartToRender = {
+          data: leData,
+          average: result.average,
+          total: result.total,
+          type: chartType
+        };
+        return response;
+      })
+    );
+  }
+
+  getAddress(topic: string, id: number | null) {
+    //["globalni broj vožnji","globalni broj kilometara","globalni troškovi"];
+    let address: string = "";
+    switch (topic) {
+      case "globalni broj vožnji":
+        address = "admin/rides";
+        break;
+      case 'globalni broj kilometara':
+        address = "admin/kilometers";
+        break;
+      case 'globalni troškovi':
+        address = 'admin/expenses';
+        break;
+      case "broj vožnji":
+        address = "passenger/"+id+"/rides";
+        break;
+      case 'broj kilometara':
+        address = "passenger/"+id+"/kilometers";
+        break;
+      case 'troškovi':
+        address = "passenger/"+id+"/expenses";
+        break;
+      case "broj obavljenih vožnji":
+        address = "driver/"+id+"/rides"
+        break;
+      case 'broj voženih kilometara':
+        address = "driver/"+id+"/kilometers";
+        break;
+      default:
+        throw new Error("chart type not found!");
+    }
+    return address;
+  }
 
 }
 
