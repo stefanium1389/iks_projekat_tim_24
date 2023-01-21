fb0dba1cf95fe3cddb3c3d0eccfacd417990b4c9
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-01-21 12:52:48 +0100

    few dtos

--- iks_projekat_tim_24_uberApp/src/app/backend-services/ride-data.service.ts
+++ iks_projekat_tim_24_uberApp/src/app/backend-services/ride-data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { DTOList } from './DTO/DTOList';
 import { RideDTO, RideRequestDTO } from './DTO/RideDTO';
 
 @Injectable({
@@ -17,5 +18,4 @@ export class RideDataService {
   getRideById(id:number):Observable<RideDTO>{
     return this.http.get<RideDTO>(environment.apiBaseUrl+`api/ride/${id}`);
   }
-
 }
