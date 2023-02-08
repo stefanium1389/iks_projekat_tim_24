59dbbd685ca2d0d9fc8eae5ee7e5663d016f52a3
VladimirGolosin <vladimirgolosin1@gmail.com>
2023-02-09 00:00:49 +0100

    feat: add tests

--- iks_projekat_tim_24_uberApp/src/app/components/register/register.component.spec.ts
+++ iks_projekat_tim_24_uberApp/src/app/components/register/register.component.spec.ts
@@ -36,25 +36,103 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('form should be invalid', () => {
-    component.registerForm.controls['email'].setValue('');
-    component.registerForm.controls['password'].setValue('');
-    component.registerForm.controls['repeatPassword'].setValue('');
-    component.registerForm.controls['name'].setValue('');
-    component.registerForm.controls['surname'].setValue('');
-    component.registerForm.controls['phone'].setValue('');
-    component.registerForm.controls['address'].setValue('');
-    expect(component.registerForm.valid).toBeFalsy();
-  });
-
-  it('form should be valid', () => {
-    component.registerForm.controls['email'].setValue('test@example.com');
+  it('form should be invalid if email doesnt have a @', () => {
+    component.registerForm.controls['email'].setValue('testexamplegmail.com');
     component.registerForm.controls['password'].setValue('password');
     component.registerForm.controls['repeatPassword'].setValue('password');
     component.registerForm.controls['name'].setValue('test');
     component.registerForm.controls['surname'].setValue('test');
     component.registerForm.controls['phone'].setValue('1234567890');
     component.registerForm.controls['address'].setValue('address');
-    expect(component.registerForm.valid).toBeTruthy();
+    expect(component.registerForm.valid).toBeFalsy();
   });
+
+  it('form should be invalid if email doesnt have a dot', () => {
+    component.registerForm.controls['email'].setValue('testexample@gmailcom');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('test');
+    component.registerForm.controls['surname'].setValue('test');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when password and repeat password do not match', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password1');
+    component.registerForm.controls['name'].setValue('test');
+    component.registerForm.controls['surname'].setValue('test');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when name is not provided', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('');
+    component.registerForm.controls['surname'].setValue('test');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when name is not provided', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('name');
+    component.registerForm.controls['surname'].setValue('');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when email is not provided', () => {
+    component.registerForm.controls['email'].setValue('');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('name');
+    component.registerForm.controls['surname'].setValue('');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when password is not provided', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('');
+    component.registerForm.controls['repeatPassword'].setValue('');
+    component.registerForm.controls['name'].setValue('name');
+    component.registerForm.controls['surname'].setValue('');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when phone is not provided', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('name');
+    component.registerForm.controls['surname'].setValue('');
+    component.registerForm.controls['phone'].setValue('');
+    component.registerForm.controls['address'].setValue('address');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when address is not provided', () => {
+    component.registerForm.controls['email'].setValue('test@example.com');
+    component.registerForm.controls['password'].setValue('password');
+    component.registerForm.controls['repeatPassword'].setValue('password');
+    component.registerForm.controls['name'].setValue('name');
+    component.registerForm.controls['surname'].setValue('');
+    component.registerForm.controls['phone'].setValue('1234567890');
+    component.registerForm.controls['address'].setValue('');
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
 })
